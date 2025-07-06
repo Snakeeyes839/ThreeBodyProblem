492206c2d255b23720b9620e2d26c79c5c419535
Joshua <34919492+Snakeeyes839@users.noreply.github.com>
2025-07-06 10:23:53 -0400

    Create dynamic motion on the UI
    The Kivy UI now displays the motion of three bodies. The bodies are
    predetermined, hardcoded values but will later be updated to allow for
    user input.
    
    Cleaned up old code to be more readable and removed the old MatPlotLib
    related code.

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import (NumericProperty, ReferenceListProperty, ObjectProperty)
@@ -10,13 +7,44 @@ from kivy.core.window import Window
 
 
 class PhysicsBody(Widget):
+    mass = NumericProperty(10)
+
     velocity_x = NumericProperty(0)
     velocity_y = NumericProperty(0)
     velocity = ReferenceListProperty(velocity_x, velocity_y)
 
-    position_x = NumericProperty(0)
-    position_y = NumericProperty(0)
-    position = ReferenceListProperty(position_x, position_y)
+    gravitational_force_x = NumericProperty(0)
+    gravitational_force_y = NumericProperty(0)
+    gravitational_force = ReferenceListProperty(gravitational_force_x, gravitational_force_y)
+
+    def set_initial_conditions(self, mass, init_v):
+        self.mass = mass
+        self.velocity = init_v
+
+    def calc_gravitational_force(self, exerting_body):
+        # G = 6.6743e−11
+        G = 1
+
+        # Newton's Law of Universal Gravitation in Vector Form
+        # Fg = -G * (m1m2 / |r_21|^3) * r_21
+        # gravitation_force is added to the new force here because if there is more than two bodies in the simulation
+        # then we need to add the totals up. But at the end of update_velocity_and_position we reset the total
+        # gravitational_force to zero to recalculate gravity based on the new positions
+        m1m2 = self.mass * exerting_body.mass
+        b1b2_displacement = Vector(self.center) - Vector(exerting_body.center)
+        b1b2_magnitude = b1b2_displacement.length()
+
+        self.gravitational_force = (-G * (m1m2 * b1b2_displacement) / (b1b2_magnitude ** 3)) + self.gravitational_force
+
+    def update_velocity_and_position(self, delta_time):
+        # Convert Force vector applied by gravity to velocity
+        self.velocity = ((Vector(*self.gravitational_force) / self.mass) * delta_time) + self.velocity
+
+        # Convert velocity into position
+        self.center = (Vector(*self.velocity) * delta_time) + self.center
+
+        # Reset the gravitational_force since positions have changed, thus changing the gravitational effects
+        self.gravitational_force = [0, 0]
 
 
 class ThreeBodySim(Widget):
@@ -25,139 +53,45 @@ class ThreeBodySim(Widget):
     body3 = ObjectProperty(None)
 
     def initialize_sim(self):
-        self.body1 = PhysicsBody(pos=(Window.width/2 + 100, Window.height/2), size=(10, 10))
-        self.body2 = PhysicsBody(pos=(Window.width/2 + 0, Window.height/2), size=(10, 10))
-        self.body3 = PhysicsBody(pos=(Window.width/2 - 100, Window.height/2), size=(10, 10))
+        window_h_center = Window.height / 2
+        window_w_center = Window.width / 2
+
+        r1 = 5
+        r2 = 40
+        r3 = 5
+
+        self.body1 = PhysicsBody(pos=(window_w_center + 100 - r1, window_h_center - r1), size=(2*r1, 2*r1))
+        self.body2 = PhysicsBody(pos=(window_w_center - r2, window_h_center - r2), size=(2*r2, 2*r2))
+        self.body3 = PhysicsBody(pos=(window_w_center - 100 - r3, window_h_center - r3), size=(2*r3, 2*r3))
+
+        self.body1.set_initial_conditions(200, Vector(0, -30))
+        self.body2.set_initial_conditions(100000, Vector(0, 0))
+        self.body3.set_initial_conditions(200, Vector(0, 30))
+
         self.add_widget(self.body1)
         self.add_widget(self.body2)
         self.add_widget(self.body3)
-        pass
 
     def update(self, dt):
-        pass
+        self.body1.calc_gravitational_force(self.body2)
+        self.body1.calc_gravitational_force(self.body3)
+        self.body2.calc_gravitational_force(self.body1)
+        self.body2.calc_gravitational_force(self.body3)
+        self.body3.calc_gravitational_force(self.body1)
+        self.body3.calc_gravitational_force(self.body2)
+
+        self.body1.update_velocity_and_position(dt)
+        self.body2.update_velocity_and_position(dt)
+        self.body3.update_velocity_and_position(dt)
 
 
 class ThreeBodyProblem(App):
     def build(self):
         sim = ThreeBodySim()
         sim.initialize_sim()
-        Clock.schedule_interval(sim.update, 1.0 / 60.0)
+        Clock.schedule_interval(sim.update, .001)
         return sim
-
-
-class Body:
-    """Establishes a physical body"""
-
-    def __init__(self, mass, initial_pos, initial_v):
-        self.mass = mass
-        self.position = initial_pos
-        self.velocity = initial_v
-        self.gravitational_force = np.array([0.0, 0.0])
-        self.position_history_x = [np.array(initial_pos[0])]
-        self.position_history_y = [np.array(initial_pos[1])]
-
-    def calc_gravitational_force(self, exterting_body):
-        G = 1
-        # G = 6.6743e−11
-
-        # Newton's Law of Universal Gravitation in Vector Form
-        # Fg = -G * (m1m2 / |r_21|^3) * r_21
-        # gravitation_force is added to the new force here because if there is more than two bodies in the simulation
-        # then we need to add the totals up. But at the end of update_velocity_and_position we reset the total
-        # gravitational_force to zero to recalculate gravity based on the new positions
-        self.gravitational_force = self.gravitational_force + -G * ((exterting_body.mass * self.mass) * (self.position - exterting_body.position)) \
-                                   / (np.linalg.norm(self.position - exterting_body.position) ** 3)
-
-    def update_velocity_and_position(self, delta_time):
-        # Convert Force vector applied by gravity to velocity
-        self.velocity = self.velocity + ((self.gravitational_force / self.mass) * delta_time)
-
-        # Convert velocity into position
-        self.position = self.position + (self.velocity * delta_time)
-
-        # Reset the gravitational_force since positions have changed, thus changing the gravitational effects
-        self.gravitational_force = [0, 0]
-
-        # Record the history for the plot animation
-        self.position_history_x.append(self.position[0])
-        self.position_history_y.append(self.position[1])
-
-
-def find_min_max_between_arrays(arr1, arr2):
-    if min(arr1) < min(arr2):
-        minimum = min(arr1)
-    else:
-        minimum = min(arr2)
-
-    if max(arr1) > max(arr2):
-        maximum = max(arr1)
-    else:
-        maximum = max(arr2)
-
-    return minimum, maximum
-
-
-# animate a plot with the gravitational data calculated
-def animate(step):
-    history1_x = body1.position_history_x[:step]
-    history1_y = body1.position_history_y[:step]
-
-    history2_x = body2.position_history_x[:step]
-    history2_y = body2.position_history_y[:step]
-
-    history3_x = body3.position_history_x[:step]
-    history3_y = body3.position_history_y[:step]
-
-    # Plot positions
-    trace_body1.set_data(history1_x, history1_y)
-    trace_body2.set_data(history2_x, history2_y)
-    trace_body3.set_data(history3_x, history3_y)
-
-    time_text.set_text(time_template % step)
-
-    return trace_body1, trace_body2, trace_body3, time_text
 
 
 if __name__ == '__main__':
     ThreeBodyProblem().run()
-else:
-    body1 = Body(2, np.array([0, 1]), np.array([-1, 0]))
-    body2 = Body(2, np.array([0, -1]), np.array([1, 0]))
-    body3 = Body(2, np.array([0, 0]), np.array([0, 0]))
-
-    frame_time = float(input("What is the frame time: "))
-    time_scale = int(input("Number of frames to calculate through: "))
-
-    for i in range(time_scale):
-        # Print the progress of the calculations
-        if (i % 100) == 0:
-            print("{}%".format(round((i / time_scale) * 100), 2))
-
-        body1.calc_gravitational_force(body2)
-        body1.calc_gravitational_force(body3)
-        body2.calc_gravitational_force(body1)
-        body2.calc_gravitational_force(body3)
-        body3.calc_gravitational_force(body1)
-        body3.calc_gravitational_force(body2)
-
-        body1.update_velocity_and_position(frame_time)
-        body2.update_velocity_and_position(frame_time)
-        body3.update_velocity_and_position(frame_time)
-
-    x_min, x_max = find_min_max_between_arrays(body1.position_history_x, body2.position_history_x)
-    y_min, y_max = find_min_max_between_arrays(body1.position_history_y, body2.position_history_y)
-
-    fig = plt.figure(figsize=(5, 5))
-    ax = fig.add_subplot(autoscale_on=False, xlim=(x_min-1, x_max+1), ylim=(y_min-1, y_max+1))
-
-    time_template = 'Frame = %d'
-    time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
-
-    # Create the trace line that is a point followed by a solid line
-    trace_body1, = ax.plot([], [], '.-', lw=1, ms=2)
-    trace_body2, = ax.plot([], [], '.-', lw=1, ms=2)
-    trace_body3, = ax.plot([], [], '.-', lw=1, ms=2)
-
-    ani = animation.FuncAnimation(fig, animate, time_scale, interval=10, blit=True)
-    ax.grid(True)
-    plt.show()
