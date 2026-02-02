06ef28f8e5d9ce3590de26a6158503f7220c2c8f
Joshua <34919492+Snakeeyes839@users.noreply.github.com>
2026-02-02 12:17:32 -0500

    Update to have multiple physics bodies
    Updated the project to have a button that added physics bodies instead of adding them on a mouse click. We can now have more than 3 objects

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+import logging
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import (NumericProperty, ReferenceListProperty, ObjectProperty)
 from kivy.vector import Vector
 from kivy.clock import Clock
 from kivy.core.window import Window
+
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+# logging.disable(logging.DEBUG)
+
+
+class BodyUI(Widget):
+    pass
 
 
 class PhysicsBody(Widget):
@@ -34,6 +43,8 @@ class PhysicsBody(Widget):
         b1b2_displacement = Vector(self.center) - Vector(exerting_body.center)
         b1b2_magnitude = b1b2_displacement.length()
 
+        assert b1b2_magnitude != 0, "Displacement should not be zero"
+
         self.gravitational_force = (-G * (m1m2 * b1b2_displacement) / (b1b2_magnitude ** 3)) + self.gravitational_force
 
     def update_velocity_and_position(self, delta_time):
@@ -48,11 +59,11 @@ class PhysicsBody(Widget):
 
 
 class ThreeBodySim(Widget):
-    body1 = ObjectProperty(None)
-    body2 = ObjectProperty(None)
-    body3 = ObjectProperty(None)
+    bodies = []
+    bodyUI = ObjectProperty(None)
+    add_button = ObjectProperty(None)
 
-    i = 0
+    click = 0
 
     def initialize_sim(self):
         window_h_center = Window.height / 2
@@ -62,39 +73,57 @@ class ThreeBodySim(Widget):
         r2 = 40
         r3 = 5
 
-        self.body1 = PhysicsBody(pos=(window_w_center + 100 - r1, window_h_center - r1), size=(2*r1, 2*r1))
-        self.body2 = PhysicsBody(pos=(window_w_center - r2, window_h_center - r2), size=(2*r2, 2*r2))
-        self.body3 = PhysicsBody(pos=(window_w_center - 100 - r3, window_h_center - r3), size=(2*r3, 2*r3))
+        #self.bodies[0] = PhysicsBody(pos=(window_w_center + 100 - r1, window_h_center - r1), size=(2*r1, 2*r1))
+        #self.bodies[1] = PhysicsBody(pos=(window_w_center - r2, window_h_center - r2), size=(2*r2, 2*r2))
+        #self.bodies[2] = PhysicsBody(pos=(window_w_center - 100 - r3, window_h_center - r3), size=(2*r3, 2*r3))
 
-        self.body1.set_initial_conditions(200, Vector(0, -30))
-        self.body2.set_initial_conditions(100000, Vector(0, 0))
-        self.body3.set_initial_conditions(200, Vector(0, 30))
+        #self.bodies[0].set_initial_conditions(200, Vector(0, -30))
+        #self.bodies[1].set_initial_conditions(100000, Vector(0, 0))
+        #self.bodies[2].set_initial_conditions(200, Vector(0, 30))
 
-        self.add_widget(self.body1)
-        self.add_widget(self.body2)
-        self.add_widget(self.body3)
+        #self.add_widget(self.bodies[0])
+        #self.add_widget(self.bodies[1])
+        #self.add_widget(self.bodies[2])
+
+        add_button = self.btn1
+        add_button.bind(on_press=self.button_callback)
 
     def update(self, dt):
-        if self.i >= 3:
-            self.body1.calc_gravitational_force(self.body2)
-            self.body1.calc_gravitational_force(self.body3)
-            self.body2.calc_gravitational_force(self.body1)
-            self.body2.calc_gravitational_force(self.body3)
-            self.body3.calc_gravitational_force(self.body1)
-            self.body3.calc_gravitational_force(self.body2)
 
-            self.body1.update_velocity_and_position(dt)
-            self.body2.update_velocity_and_position(dt)
-            self.body3.update_velocity_and_position(dt)
+        if self.click >= 3:
+            # Applied Bodies
+            for i in range(len(self.bodies)):
+                # Exerting Bodies
+                for j in range(len(self.bodies)):
+                    if i is not j:
+                        logging.debug('Calculating gravity for applied body {0} from exerting body {1}'.format(i, j))
+                        self.bodies[i].calc_gravitational_force(self.bodies[j])
+                    else:
+                        logging.debug('Skipping gravity of {0} from {1}. Dont apply gravity to self'.format(i, j))
 
-    def on_touch_down(self, touch):
-        if self.i == 0:
-            self.body1.center = touch.pos
-        elif self.i == 1:
-            self.body2.center = touch.pos
-        elif self.i == 2:
-            self.body3.center = touch.pos
-        self.i = self.i + 1
+            for body in self.bodies:
+                logging.debug('Updating velocity and position')
+                body.update_velocity_and_position(dt)
+
+    def button_callback(self, event):
+        window_h_center = Window.height / 2
+        window_w_center = Window.width / 2
+
+        self.bodies.append(ObjectProperty(None))
+
+        self.bodies[self.click] = PhysicsBody(pos=(window_w_center + 50*self.click, window_h_center), size=(10, 10))
+        self.bodies[self.click].set_initial_conditions(200, Vector(0, -30 * self.click))
+
+        self.add_widget(self.bodies[self.click])
+
+        logging.debug(len(self.bodies))
+
+        self.click += 1
+
+    # def on_touch_down(self, touch):
+    #     if self.click < 3:
+    #         self.bodies[self.click].center = touch.pos
+    #         self.click = self.click + 1
 
 
 class ThreeBodyProblem(App):
