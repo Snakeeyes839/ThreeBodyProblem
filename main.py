778e57d87cdd4126a27f868bd255d118502610d5
Joshua <34919492+Snakeeyes839@users.noreply.github.com>
2026-02-05 10:50:34 -0500

    Update UI, remove objects, and update edge cases
    Added delete button to remove objects
    
    UI has been updated to rearrange when an object is removed
    
    Sim has a max of 6 object for now so that all UI elements fit onto the 1920x1080 screen size. Todo: auto scale and move elements to all fit the screen, or add minimization
    
    Edge case of zero mass causing divide by zero error accounted for
    
    Edge case of zero radius not being valid for rending accounted for
    
    Two objects placed on the same space now get bumped a little to avoid divide by zero error, since displacement would be zero
    
    Initial conditions now take values from the associated UI
    
    Added min/max variables for the randomized values

--- main.py
+++ main.py
@@ -8,25 +8,38 @@ Config.set('graphics', 'height', 1080)
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
 from kivy.properties import (NumericProperty, ReferenceListProperty, ObjectProperty)
 from kivy.vector import Vector
 from kivy.clock import Clock
 from kivy.core.window import Window
 import random
-import re
 
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-# logging.disable(logging.DEBUG)
+logging.disable(logging.DEBUG)
+
+MIN_POS_X, MAX_POS_X = -500, 500
+MIN_POS_Y, MAX_POS_Y = -300, 300
+MIN_VEL_X, MAX_VEL_X = -100, 100
+MIN_VEL_Y, MAX_VEL_Y = -100, 100
+MIN_R, MAX_R = 1, 50
+MIN_M, MAX_M = 10, 100000
+
+# Later add adjusting UI so more fit of the screen
+MAX_ON_SCREEN = 6
 
 
 # Input is restricted to numeric, but we still need to account for null or '-'
 def to_int(value):
-    if value is '-' or value is '':
+    if value == '-' or value == '':
         return 0
     else:
         return int(value)
+
+
+def rearrange_ui(bodies):
+    for i in range(len(bodies)):
+        bodies[i].ui.grid.pos = (10, Window.height - (bodies[i].ui.grid.height * (i + 1)))
 
 
 class BodyUI(Widget):
@@ -44,13 +57,18 @@ class PhysicsBody(Widget):
     gravitational_force_y = NumericProperty(0)
     gravitational_force = ReferenceListProperty(gravitational_force_x, gravitational_force_y)
 
-    def set_initial_conditions(self, mass, init_v):
-        self.mass = abs(mass)
-        self.velocity = init_v
-
-    def update_initial_conditions(self, ui_info):
-        mass = abs(to_int(ui_info.mass.text))
+    def set_initial_conditions(self, ui_info):
+        mass   = abs(to_int(ui_info.mass.text))
         radius = abs(to_int(ui_info.radius.text))
+
+        # To avoid any zero mass that would cause divide by zero error
+        if mass == 0:
+            mass = 1
+
+        # To avoid any render issues
+        if radius == 0:
+            radius = 1
+
         init_v = (to_int(ui_info.velocity_x.text), to_int(ui_info.velocity_y.text))
 
         x = (Window.width / 2) + to_int(ui_info.position_x.text)
@@ -62,7 +80,7 @@ class PhysicsBody(Widget):
         self.center = (x, y)
 
     def calc_gravitational_force(self, exerting_body):
-        # G = 6.6743e−113
+        # G = 6.6743e−11
         G = 6.6743
 
         # Newton's Law of Universal Gravitation in Vector Form
@@ -74,7 +92,9 @@ class PhysicsBody(Widget):
         b1b2_displacement = Vector(self.center) - Vector(exerting_body.center)
         b1b2_magnitude = b1b2_displacement.length()
 
-        assert b1b2_magnitude != 0, "Displacement should not be zero"
+        # In the event that two object are occupying the same space nudge them along
+        if b1b2_magnitude == 0:
+            b1b2_magnitude = 1
 
         self.gravitational_force = (-G * (m1m2 * b1b2_displacement) / (b1b2_magnitude ** 3)) + self.gravitational_force
 
@@ -94,17 +114,15 @@ class ThreeBodySim(Widget):
     bodyUI = []
     add_button = ObjectProperty(None)
 
-    click = 0
     is_paused = True
 
     def initialize_sim(self):
         add_button = self.add_btn
-        play_btn = self.play_btn
+        play_btn   = self.play_btn
         add_button.bind(on_press=self.add_callback)
         play_btn.bind(on_press=self.pause_play_callback)
 
     def update(self, dt):
-
         if not self.is_paused:
             # Applied Bodies
             for applied_body in self.bodies:
@@ -121,53 +139,67 @@ class ThreeBodySim(Widget):
                 body.update_velocity_and_position(dt)
         else:
             for i in range(len(self.bodies)):
-                self.bodies[i].update_initial_conditions(self.bodies[i].ui)
-            pass
+                self.bodies[i].set_initial_conditions(self.bodies[i].ui)
 
     def add_callback(self, event):
+        count = len(self.bodies)
+
+        # Later add adjusting UI so more fit of the screen
+        if count >= MAX_ON_SCREEN:
+            return
+
         window_h_center = Window.height / 2
         window_w_center = Window.width / 2
 
         self.bodies.append(ObjectProperty(None))
 
-        position = [random.randint(-500, 500), random.randint(-300, 300)]
-        velocity = [random.randint(-20, 20), random.randint(-20, 20)]
-        radius = 20
-        mass = 20000
+        # Randomly get Object parameters
+        position_x = random.randint(MIN_POS_X, MAX_POS_X)
+        position_y = random.randint(MIN_POS_Y, MAX_POS_Y)
+        velocity_x = random.randint(MIN_VEL_X, MAX_VEL_X)
+        velocity_y = random.randint(MIN_VEL_Y, MAX_VEL_Y)
+        radius = random.randint(MIN_R, MAX_R)
+        mass = random.randint(MIN_M, MAX_M)
 
-        x = window_w_center + position[0]
-        y = window_h_center + position[1]
+        x = window_w_center + position_x
+        y = window_h_center + position_y
 
         body = PhysicsBody(pos=(x, y), size=(2*radius, 2*radius))
+        body.ui.grid.pos = (10, Window.height - (body.ui.grid.height * (count+1)))
 
-        body.set_initial_conditions(mass, Vector(velocity[0], velocity[1]))
-
-        body.ui.grid.pos = (10, Window.height - (150 * (self.click+1)))
-
+        # Add the widget to the sim and to the list of bodies
         self.add_widget(body)
-        self.bodies[self.click] = body
+        self.bodies[count] = body
 
-        body.ui.position_x.text = str(position[0])
-        body.ui.position_y.text = str(position[1])
-
-        body.ui.velocity_x.text = str(velocity[0])
-        body.ui.velocity_y.text = str(velocity[1])
-
-        body.ui.mass.text = str(mass)
+        # Set the UI to display Object parameters
+        body.ui.position_x.text = str(position_x)
+        body.ui.position_y.text = str(position_y)
+        body.ui.velocity_x.text = str(velocity_x)
+        body.ui.velocity_y.text = str(velocity_y)
+        body.ui.mass.text   = str(mass)
         body.ui.radius.text = str(radius)
+
+        body.set_initial_conditions(body.ui)
+
+        # Bind the delete button
+        body.ui.delete_btn.bind(on_press=lambda event: self.delete_object_callback(event, body, self.bodies))
 
         # Without this the ellipse physics bodies don't appear for some reason
         self.add_widget(Label(text=""))
-
-        self.click += 1
 
     def pause_play_callback(self, event):
         self.is_paused = not self.is_paused
 
         if self.is_paused:
-            self.play_btn.text = "Play"
+            self.play_btn.text = 'Play'
         else:
-            self.play_btn.text = "Pause"
+            self.play_btn.text = 'Pause'
+
+    def delete_object_callback(self, event, body, bodies):
+        self.remove_widget(body)
+        if body in bodies:
+            bodies.remove(body)
+            rearrange_ui(bodies)
 
 
 class ThreeBodyProblem(App):
